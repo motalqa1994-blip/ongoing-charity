f61678b1f1793a684729af76923ebcffb6c12422
motalqa1994-blip <motalqa1994@gmail.com>
2025-12-06 00:40:56 +0300

    Update script.js

--- script.js
+++ script.js
@@ -14,8 +14,7 @@ const wavesurfer = WaveSurfer.create({
 });
 
 // تحميل الملف الصوتي
-wavesurfer.load('assets/audio.mp3');
-
+        wavesurfer.load('https://drive.google.com/uc?export=download&id=15w3QhjxczkhJJSof7VNpo5JoxnxVErte');
 // عناصر التحكم
 const playPauseBtn = document.getElementById('playPauseBtn');
 const timeDisplay = document.getElementById('time-display');
